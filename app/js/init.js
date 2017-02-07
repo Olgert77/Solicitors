85213f0db4cd0645e414d0b3a86f8df7f8a9b0e3
olgert77 <olgert1203@gmail.com>
2017-02-08 01:25:44 +0200

    застилизировал формстайлер, поправил слайдер

--- app/js/init.js
+++ app/js/init.js
@@ -2,16 +2,18 @@
 $('.services-slider').slick({
   slidesToShow: 6,
   slidesToScroll: 1,
-  autoplay: true,
+  autoplay: false,
+  centerMode: true,
   autoplaySpeed: 2000,
 });
 
 //init formstyler
 
+
 (function($) {
 	$(function() {
 
-  $('input, select').styler();
+  $('select').styler();
 
 });
 })(jQuery);
